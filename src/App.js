b55385c1545532f8423649a3fdf1a2d887aaac0f
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-12-16 17:16:50 -0800

    Added some get requests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+const MOVIEDB_KEY = process.env.MOVIEDB_KEY;
+const url = 'http://localhost:3000/';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      searchResults: []
+    }
+  }
+
+  getSpecificMovie = (movieTitle) => {
+    axios.get(url + 'movies?query=' + movieTitle)
+      .then(response => {
+        console.log(response);
+        this.setState({
+          searchResults: response.data
+        })
+    })
+    
+  }
+
+  componentDidMount() {
+    this.getSpecificMovie('Jaws 3-D');
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +38,22 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <p>
+            All the Get/Search requests:
+          </p>
+          <p>
+            Get list of movies:
+            <code>GET /movies</code>
+          </p>
+          <p>
+            Get a specific movie:
+            <code>GET /movies?query=Psycho</code>
+          </p>
+          <p>
+            {this.state.searchResults.length > 0 ? this.state.searchResults[0].title : 'none!'}
+          </p>
+        </div>
       </div>
     );
   }
